5eb6b357efd2ae179a8940f94cd095d789c4278d
HiperBrainer <brainerreniarb@gmail.com>
2022-08-17 11:02:33 -0300

    ADD the functions in cpp

--- abbb.cpp
+++ abbb.cpp
@@ -15,5 +15,16 @@ typedef struct node{
 
 typedef node *avl;
 
-
+node *NewNode(key id, rrn rrn);
+avl rotationRight(avl r);
+avl rotationLeft(avl r);
+avl insertAVL(node *r, node *novo, int *increased_height);
+avl insert(avl r, key id, rrn rrn);
+avl AVLmax(avl r);
+avl AVLmin(avl r);
+avl balancing_left(avl r, int *decreased_height);
+avl balancing_right(avl r, int *decreased_height);
+avl search_remove(avl r, avl node_key, int *decreased_height);
+avl removeAVL(avl r, key id, int *decreased_height);
+avl remove(avl r, key id);
 
